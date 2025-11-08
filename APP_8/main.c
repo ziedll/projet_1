985d8fa35bea69f148d66f0f3825057b78c1c5b8
ziedll <z.darragi2@gmail.com>
2025-11-08 16:32:28 +0100

    Remove unused comment

--- APP_8/main.c
+++ APP_8/main.c
@@ -67,7 +67,6 @@ int main()
                 }
                 else
                 {
-                    //printf(p[n].repas);
                     p[n].repas=0;
                 }
             }
